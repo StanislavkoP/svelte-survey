4c3498a3067c9bf6a83420453bc77b3f591bc313
StanislavkoP <stas8327634@gmail.com>
2019-05-27 17:58:43 +0300

    Refactored code

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,24 @@
 <script>
     import { onMount, afterUpdate } from 'svelte';
 
+    import Header from './Header.svelte';
     import QuestionaryComponent from './Questionary.svelte';
 	import { Questionary } from './QuestionatyModel.js'
 	
     let surveyConfig = null;
+    let nameVacancy = '';
 
     onMount(() => {
-        fetch('/page.json')
+        const typeVacancy = window.location.hash.slice(1);
+        
+        switch (typeVacancy) {
+            case 'prorab' : 
+                nameVacancy = "прораба";
+                break;
+            default: return;
+        }
+
+        fetch(`/${typeVacancy}.json`)
             .then(data => data.json())
             .then(response => {
                 surveyConfig = new Questionary(response);
@@ -20,9 +31,18 @@
 <style>
     :global(body) {
         font-family: 'Roboto', sans-serif;
+        padding: 0;
     }
+
+    #survey {
+        width: 100%;
+        max-width: 768px;
+        margin: 0 auto;
+    }
+
 </style>
 
+<Header nameVacancy={nameVacancy}/>
 <div id="survey">
     <QuestionaryComponent survey={surveyConfig}/>
 </div>
