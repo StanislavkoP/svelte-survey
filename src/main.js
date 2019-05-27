80ce32d48bd2a6579ca383fa813473c238aa2397
StanislavkoP <stas8327634@gmail.com>
2019-05-27 17:57:43 +0300

    Refactored code

--- src/main.js
+++ src/main.js
@@ -1,24 +1,5 @@
 import App from './App.svelte';
 
-
-// if (localStorage.jwtToken) {
-// 	setAuthToken(localStorage.jwtToken);
-
-// 	const jwtTokenDecoded = jwt_decode(localStorage.jwtToken);
-
-// 	const currentTime = Date.now() / 1000;
-
-// 	if (jwtTokenDecoded.exp < currentTime) {	
-// 		store.dispatch( actions.clearCurrentProfile() );
-// 		store.dispatch( actions.submitLogOut() );
-	
-// 	} else {
-// 		store.dispatch( actions.submitLogInSuccess(jwtTokenDecoded) );
-
-// 	}
-
-// } 
-
 const app = new App({
 	target: document.body,
 });
