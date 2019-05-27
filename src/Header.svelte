5001e57d5a8f76da1ae3a5515e67094f6a0f0b4c
StanislavkoP <stas8327634@gmail.com>
2019-05-27 17:58:58 +0300

    Added header

---
+++ src/Header.svelte
@@ -0,0 +1,29 @@
+<header class="main-header">
+    <a class="main-header__logo" href="http://petrishin.ru/">
+        <img src="./images/logo.png" alt="петришин-строй"  class="main-header__logo-img">
+    </a>
+
+    <h1 class="main-header__title">{ `Анкета на вакансию ${nameVacancy}` }</h1>
+</header>
+
+<script>
+    export let nameVacancy = '';
+</script>
+
+<style>
+    .main-header {
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid black;
+    }
+
+    .main-header__logo {
+        width: 100%;
+        max-width: 360px;
+    }
+
+    .main-header__logo-img {
+        width: 100%;
+    }
+
+</style>
