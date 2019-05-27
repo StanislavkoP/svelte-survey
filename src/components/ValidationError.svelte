ee72600b62e2b2191270b9c3e6b9efb3b3887f93
StanislavkoP <stas8327634@gmail.com>
2019-05-27 17:46:22 +0300

    Added error component

---
+++ src/components/ValidationError.svelte
@@ -0,0 +1,26 @@
+{#if isShowed}
+    <div class="error">
+        <div class="error-text">{ errorText }</div>
+    </div>
+{/if}
+
+
+<script>
+    export let isShowed = false;
+    export let errorText = '';
+</script>
+
+<style>
+    .error-text {
+        padding: 12px 10px;
+        width: 100%;
+    }
+
+    .error {
+        font-size: 14px;
+        padding: 6px;
+        color: white;
+        background-color: rgba(245, 70, 52, 0.9);
+    }
+
+</style>
